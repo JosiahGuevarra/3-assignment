0ada95baf1afff33cc9a09335f65785b89ea0ca7
Josiah Guevarra <701031808@GoTVDSB.ca>
2025-02-12 14:24:44 -0500

    new changes made

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.BackgroundColor = ConsoleColor.Magenta;
-            int room;
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.BackgroundColor = ConsoleColor.White;
+            int room, length, weidth;
             double price;
             string subject;
             string name;
@@ -17,8 +17,10 @@
             subject = "Computer science!";
             name = " Josiah Guevarra";
             grad = 2026;
-            radius = 3;
+            radius = 9;
             pi = 3.14;
+            length = 8;
+            weidth = 9;
 
 
             Console.WriteLine(" The room number is " + room);
@@ -27,7 +29,8 @@
             Console.WriteLine(" My name is" + name + (" and I Graduate in " +  grad));
             Console.WriteLine(" The Radius is " + radius);
             Console.WriteLine(" The area of this circle is " + (Math.Round(radius * radius * pi)));
-
+            Console.WriteLine(" the formula of a square is length times weidth");
+            Console.WriteLine(" the length equals 8 and the weidth equals 9," + (" this means the area of this square equals ") + (length * weidth));
             
             
         }
